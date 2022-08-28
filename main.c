247db029813aff12cd16994c6d8a867737c4b539
Alexaandrepaiva <101399070+Alexaandrepaiva@users.noreply.github.com>
2022-08-28 09:25:34 -0300

    Inventórios e protótipos

--- main.c
+++ main.c
@@ -1,11 +1,11 @@
-// inclus�o das bibliotecas
+// inclusão das bibliotecas
 #include<stdio.h>
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
 #include<locale.h>
 
-// tamanhos m�ximos pr�-definidos
+// tamanhos máximos pré-definidos
 #define MAX 10
 #define SIZE 20
 
@@ -13,7 +13,7 @@
 #define NOME_ARQUIVO_ENTRADA "entrada.txt"
 #define NOME_ARQUIVO_SAIDA "saida.txt"
 
-// tipos de n�s que existem
+// tipos de nós que existem
 typedef enum _tipoNo
 {
     raiz,
@@ -28,7 +28,7 @@ typedef enum _itensEssenciais
     lanterna,
     caboSolteiro,
     caderno,
-    itemEssencial // item gen�rico para inicializar o invent�rio
+    itemEssencial // item genérico para inicializar o invent�rio
 
 } itensEssenciais;
 
@@ -38,17 +38,17 @@ typedef enum _itensRaros
     malvinao,
     barrinhaDeCereal,
     faca,
-    itemRaro // item gen�rico para inicializar o invent�rio
+    itemRaro // item genérico para inicializar o invent�rio
 } itensRaros;
 
-// vari�vel que indica a op��o que eu selecionei em um n� e para qual n� essa op��o leva
+// variável que indica a opção que eu selecionei em um nó e para qual nó essa oção leva
 typedef struct _opcao
 {
     char opcaoSelecionada;
     int indiceProximoNo;
 } opcao;
 
-// n� propriamente dito e suas caracter�sticas
+// nó propriamente dito e suas características
 typedef struct _no
 {
     int indice;
@@ -60,15 +60,39 @@ typedef struct _no
     struct _no *proximo;
 } no;
 
-// ponteiros de n�s que apontam para o n� raiz e o ponteiro que estamos atualmente
+// ponteiros de nós que apontam para o nó raiz e o ponteiro que estamos atualmente
 no *ptrInicio;
 no *ptrAtual;
 
-// arquivo de sa�da
+// arquivo de saída
 FILE *arquivoSaida;
 
-// vari�veis globais
+// variáveis globais
 int moedas;
 int counterFO = 0;
 int counterAtividades = 0;
 int counterItensRaros = 0;
+
+// inventórios
+itensEssenciais inventarioEssencial[6] = {itemEssencial, itemEssencial, itemEssencial, itemEssencial, itemEssencial, itemEssencial};
+itensRaros inventarioRaro[3] = {itemRaro, itemRaro, itemRaro};
+
+// protótipos
+char *lerNomeJogador(char *nomeArquivo);
+void cadastrarNo(int indice, tipoNo tipo, opcao *opcoes, int nOpcoes, char *texto);
+void cadastrarNos();
+no *buscarNo(int indice);
+int lerIndiceProximoNo(char opcao);
+int isCompra(int indice);
+int compra(int indice, int moedas, char opcao, itensEssenciais *inventarioEssencial, itensRaros *inventarioRaro);
+int isValendoFO(int indice);
+int isValendoItem(int indice);
+int verificarItemEssencial(int indice, itensEssenciais *inventarioEssencial);
+int verificarItemRaro(int indice, itensRaros *inventarioRaro);
+int isFoundable(int indice);
+void addItemRaro(itensRaros *inventarioRaro, int indice);
+void addItemEssencial(itensEssenciais *inventarioEssencial);
+void limparTela();
+void readItensRaros(itensRaros *inventarioRaro, int *counterItensRaros, int *counterFO);
+void printInventarioEssencial(itensEssenciais *inventarioEssencial);
+void printInventarioRaro(itensRaros *inventarioRaro);
